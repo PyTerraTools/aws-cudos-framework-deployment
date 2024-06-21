eda9bffccea4eaf29f79e055aed4436cd7f7ddb4
mazondel <lennon.mazonde@post.ch>
2024-06-21 11:43:21 +0200

    created stack iam policy with glue logs and s3 permissions, created conditional statement to use policy dta source is variable is null

--- terraform-modules/one-click-terraform/modules/destination_account/stack_iam_role.tf
+++ terraform-modules/one-click-terraform/modules/destination_account/stack_iam_role.tf
@@ -1,13 +1,29 @@
-# data "aws_iam_policy_document" "stack_iam_role" {
-#   statement {
-#     actions = [
-#       "glue:GetDatabase",
-#       "logs:PutLogEvents",
-#       "s3:Get*"
-#     ]
-#     resources = ["*"]
-#   }
-# }
+data "aws_iam_policy_document" "stack_iam_role" {
+  statement {
+    actions = [
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws-glue/crawlers:*"]
+  }
+
+  statement {
+    actions = [
+      "glue:GetDatabase"
+    ]
+    resources = ["arn:aws:glue:us-east-1:${data.aws_caller_identity.current.account_id}:catalog/database/cid_cur"]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::cid-${data.aws_caller_identity.current.account_id}-shared",
+      "arn:aws:s3:::cid-${data.aws_caller_identity.current.account_id}-shared/cur/*"
+    ]
+  }
+}
 
 # resource "aws_iam_role" "stack_iam_role" {
 #   name               = "CidCloudFormationStackRole"
