3ed011f541d521e91b2440c03049c60ccde16cfd
mazondel <lennon.mazonde@post.ch>
2024-06-21 12:28:26 +0200

    put stack policy body in locals valuie

--- terraform-modules/one-click-terraform/modules/destination_account/stack_iam_role.tf
+++ terraform-modules/one-click-terraform/modules/destination_account/stack_iam_role.tf
@@ -1,31 +1,33 @@
-data "aws_iam_policy_document" "stack_policy_body" {
-  statement {
-    actions = [
-      "logs:PutLogEvents"
-    ]
-    resources = ["arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws-glue/crawlers:*"]
-  }
-
-  statement {
-    actions = [
-      "glue:GetDatabase"
-    ]
-    resources = ["arn:aws:glue:us-east-1:${data.aws_caller_identity.current.account_id}:catalog/database/cid_cur"]
-  }
-
-  statement {
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket"
-    ]
-    resources = [
-      "arn:aws:s3:::cid-${data.aws_caller_identity.current.account_id}-shared",
-      "arn:aws:s3:::cid-${data.aws_caller_identity.current.account_id}-shared/cur/*"
-    ]
-  }
+locals {
+  stack_policy_body = <<EOF
+{
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws-glue/crawlers:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "glue:GetDatabase"
+      ],
+      "Resource": "arn:aws:glue:us-east-1:${data.aws_caller_identity.current.account_id}:catalog/database/cid_cur"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      "Resource": [
+        "arn:aws:s3:::cid-${data.aws_caller_identity.current.account_id}-shared",
+        "arn:aws:s3:::cid-${data.aws_caller_identity.current.account_id}-shared/cur/*"
+      ]
+    }
+  ]
 }
-
-# resource "aws_iam_role" "stack_iam_role" {
-#   name               = "CidCloudFormationStackRole"
-#   assume_role_policy = data.aws_iam_policy_document.cid_stack_assume_role_policy.json
-# }
+EOF
+}
