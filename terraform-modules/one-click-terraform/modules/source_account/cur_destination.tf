a3f1c91fc18a5e407f653154f544f4fbd3ec5865
mazondel <lennon.mazonde@post.ch>
2024-06-25 12:25:57 +0200

    removed commented code

--- terraform-modules/one-click-terraform/modules/source_account/cur_destination.tf
+++ terraform-modules/one-click-terraform/modules/source_account/cur_destination.tf
@@ -1,14 +1,9 @@
-# Configure exactly one destination account
 module "cur_destination" {
-  source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cur-setup-destination?ref=0.3.4"
+  source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cur-setup-destination"
 
   source_account_ids = var.source_account_ids
-  create_cur         = var.create_cur # Set to true to create an additional CUR in the aggregation account
-  # cur_name_suffix    = var.cur_name_suffix
-  # resource_prefix    = var.resource_prefix
+  create_cur         = var.create_cur
 
-  # Provider alias for us-east-1 must be passed explicitly (required for CUR setup)
-  # Optionally, you may pass the default aws provider explicitly as well
   providers = {
     aws.useast1 = aws.useast1
   }
--- terraform-modules/one-click-terraform/modules/source_account/cur_source.tf
+++ terraform-modules/one-click-terraform/modules/source_account/cur_source.tf
@@ -1,13 +1,8 @@
-# Configure one or more source (payer) accounts
 module "cur_source" {
-  source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cur-setup-source?ref=0.3.4"
+  source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cur-setup-source"
 
   destination_bucket_arn = module.cur_destination.cur_bucket_arn
-  # cur_name_suffix        = var.cur_name_suffix
-  # resource_prefix        = var.resource_prefix
 
-  # Provider alias for us-east-1 must be passed explicitly (required for CUR setup)
-  # Optionally, you may pass the default aws provider explicitly as well
   providers = {
     aws.useast1 = aws.useast1
   }
